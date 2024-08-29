ef051056bf9492b343092bfe18a78cff0f613f5b
Diego Nicolas <167242760+DiegoDiazR@users.noreply.github.com>
2024-08-29 13:43:44 -0500

    Add files via upload

---
+++ Index/src/test/java/com/Byg/Index/IndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Byg.Index;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
